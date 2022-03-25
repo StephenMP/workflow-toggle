d9f795fd01e5add39fa52e6bb33b392015b487e1
StephenMP <1.stephen.porter@gmail.com>
2022-03-25 15:26:10 -0600

    Initial Commit

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,88 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
 import {expect, test} from '@jest/globals'
+import {fail} from 'assert'
+import nock from 'nock'
+import {v4} from 'uuid'
+import {main, ToggleAction} from '../src/main'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+const getKeyValue =
+  <U extends keyof T, T extends object>(key: U) =>
+  (obj: T) =>
+    obj[key]
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+type TestInputs = {
+  INPUT_TOKEN?: string
+  INPUT_WORKFLOW_ID?: string
+  INPUT_ACTION?: ToggleAction
+  INPUT_OWNER?: string
+  INPUT_REPO?: string
+}
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
+function setEnv(inputs: TestInputs) {
+  for (const key in inputs) {
+    process.env[key] = getKeyValue<keyof TestInputs, TestInputs>(
+      key as keyof TestInputs
+    )(inputs)
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+}
+
+function pass() {
+  expect(true).toBe(true)
+}
+
+async function run(inputs: TestInputs) {
+  setEnv(inputs)
+  await main()
+}
+
+function nockWorkflowToggleCall(
+  inputs: TestInputs,
+  toggleAction: ToggleAction,
+  shouldSucceed: boolean = true
+): nock.Scope {
+  const resource = `/repos/${inputs.INPUT_OWNER}/${inputs.INPUT_REPO}/actions/workflows/${inputs.INPUT_WORKFLOW_ID}/${toggleAction}`
+  return nock('https://api.github.com')
+    .put(resource)
+    .reply(shouldSucceed ? 200 : 500)
+}
+
+test('should enable workflow', async () => {
+  const inputs: TestInputs = {
+    INPUT_TOKEN: v4(),
+    INPUT_ACTION: 'enable',
+    INPUT_WORKFLOW_ID: v4(),
+    INPUT_OWNER: v4(),
+    INPUT_REPO: v4()
+  }
+
+  const nockScope = nockWorkflowToggleCall(inputs, 'enable')
+
+  try {
+    await run(inputs)
+    pass()
+  } catch (e) {
+    fail(e instanceof Error ? (e as Error) : 'Unknown')
+  } finally {
+    nockScope.done()
+  }
+})
+
+test('should disable workflow', async () => {
+  const inputs: TestInputs = {
+    INPUT_TOKEN: v4(),
+    INPUT_ACTION: 'disable',
+    INPUT_WORKFLOW_ID: v4(),
+    INPUT_OWNER: v4(),
+    INPUT_REPO: v4()
+  }
+
+  const nockScope = nockWorkflowToggleCall(inputs, 'disable')
+
+  try {
+    await run(inputs)
+    pass()
+  } catch (e) {
+    fail(e instanceof Error ? (e as Error) : 'Unknown')
+  } finally {
+    nockScope.done()
+  }
 })
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,71 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
-async function run(): Promise<void> {
+export type ToggleAction = 'enable' | 'disable'
+type Inputs = {
+  token: string
+  workflow_id: string
+  action: ToggleAction
+  owner: string
+  repo: string
+}
+
+const keys = {
+  token: 'token',
+  workflow_id: 'workflow_id',
+  action: 'action',
+  owner: 'owner',
+  repo: 'repo'
+}
+
+function parseInputs(): Inputs {
+  core.info('Parsing inputs')
+  const inputs: Inputs = {
+    token: core.getInput(keys.token, {required: true}),
+    workflow_id: core.getInput(keys.workflow_id, {required: true}),
+    action: core.getInput(keys.action, {required: true}) as ToggleAction,
+    owner: core.getInput(keys.owner) || github.context.repo.owner,
+    repo: core.getInput(keys.repo) || github.context.repo.repo
+  }
+
+  core.info(
+    `Inputs: ${JSON.stringify(
+      {
+        ...inputs,
+        githubToken: `${inputs.token.substring(0, 5)}...`
+      },
+      null,
+      2
+    )}`
+  )
+
+  return inputs
+}
+
+export async function main(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const {token, action, workflow_id, owner, repo} = parseInputs()
+    const octokit = github.getOctokit(token)
+    const requestBody = {owner, repo, workflow_id}
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    if (action === 'enable') {
+      core.info('Enabling workflow')
+      await octokit.rest.actions.enableWorkflow(requestBody)
+    } else if (action === 'disable') {
+      core.info('Disabling workflow')
+      await octokit.rest.actions.disableWorkflow(requestBody)
+    } else {
+      throw new Error(`${action} is an unsupported toggle action!`)
+    }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    throw error
   }
 }
 
-run()
+main()
+  .then(() => core.info('Done :)'))
+  .catch(error => {
+    if (error instanceof Error) {
+      core.setFailed(error)
+    }
+  })
